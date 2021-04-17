9db7074b2ecf82036324808141083a49161a780c
davidetacchini <davidetacchini11@gmail.com>
2021-04-17 16:36:14 +0200

    format

--- 012/sol.cpp
+++ 012/sol.cpp
@@ -4,12 +4,12 @@
 int main() {
     const int limit = 500;
     int number = 0;
-	int i = 1;
+    int i = 1;
     int factors;
 
     while(factors < limit) {
-		number += i;
-		i++;
+        number += i;
+        i++;
         factors = 0;
         for (int j = 1; j <= sqrt(number); j++) {
             if (number % j == 0) {
