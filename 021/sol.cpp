dfd785c3525e5855f523f128d532b09588884d5d
davidetacchini <davidetacchini11@gmail.com>
2021-04-24 15:50:49 +0200

    add problem 021

---
+++ 021/sol.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int sum_of_factors(int number) {
+    int sum = 0;
+    for (int i = 1; i < number; i++) {
+        if (number % i == 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+int main() {
+    const int limit = 10000;
+    int sum, sum1;
+    int result = 0;
+
+    for (int i = 1; i < limit; i++) {
+        sum = sum_of_factors(i);
+        sum1 = sum_of_factors(sum);
+        if (i == sum1 && sum != sum1) {
+            result += i;
+        }
+    }
+    std::cout << result;
+}
