e0cbde33a902a0d8a2e8e2d2ea65b97a8fe0665f
davidetacchini <davidetacchini11@gmail.com>
2021-05-01 22:34:38 +0200

    add problem 35

---
+++ 035/sol.py
@@ -0,0 +1,25 @@
+def is_prime(n):
+    if n <= 3:
+        return n > 1
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+res = 1 # counting 1 for number 2
+for i in range(3, 10**6, 2):
+    count = 0
+    if is_prime(i):
+        s = str(i)
+        for i in range(len(s)):
+            circle = int(s[i:] + s[:i])
+            if is_prime(circle):
+                count += 1
+        if count == len(s):
+            res += 1
+
+print(res)
