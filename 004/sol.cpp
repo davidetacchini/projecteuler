b272a4e0b5e32f93109ff7c5b0a68ea18b402059
davidetacchini <davidetacchini11@gmail.com>
2021-05-03 13:21:06 +0200

    <= to <

--- 004/sol.cpp
+++ 004/sol.cpp
@@ -2,11 +2,11 @@
 #include "../utils/utils.hpp"
 
 int main() {
-    const int n = 999;
+    const int n = 1000;
     int mul = 0;
     int res = 0;
     
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i < n; i++) {
         for (int j = 1; j <= n; j++) {
             mul = i * j;
             if (is_palindrome(mul) && mul > res) {
