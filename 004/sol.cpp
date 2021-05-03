03bbd08b9104b3cad030c6bfd66ccbea8584b4aa
davidetacchini <davidetacchini11@gmail.com>
2021-05-03 13:24:29 +0200

    start from 3-digits number

--- 004/sol.cpp
+++ 004/sol.cpp
@@ -6,8 +6,8 @@ int main() {
     int mul = 0;
     int res = 0;
     
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j <= n; j++) {
+    for (int i = 100; i < n; i++) {
+        for (int j = 100; j < n; j++) {
             mul = i * j;
             if (is_palindrome(mul) && mul > res) {
                 res = mul;
